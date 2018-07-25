5756eccc296add313ccab485d450ae2418f1ac1b
Mohammad Nadzif <demihuman@live.com>
2018-07-25 14:53:12 +0700

    create behavior for owner

--- src/OwnerBehavior.php
+++ src/OwnerBehavior.php
@@ -1,15 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nadzif Glovory
- * Date: 7/25/2018
- * Time: 1:04 PM
- */
 
 namespace nadzif\behaviors;
 
 
-class OwnerBehavior
+use nadzif\behaviors\helpers\IdentityHelper;
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+/**
+ * Class OwnerBehavior
+ *
+ * @package nadzif\behaviors
+ */
+class OwnerBehavior extends Behavior
 {
+    public $createdAttribute = 'createdBy';
+    public $updatedAttribute = 'updatedBy';
+    public $deletedAttribute = 'deletedBy';
+
+    public $recordWhileCreate = true;
+    public $recordWhileUpdate = true;
+    public $recordWhileDelete = true;
+
+    /**
+     * @return array
+     */
+    public function events()
+    {
+        $behaviorEvents = [];
+        if ($this->recordWhileCreate) {
+            $behaviorEvents[ActiveRecord::EVENT_AFTER_INSERT] = 'createdBy';
+        }
+
+        if ($this->recordWhileCreate) {
+            $behaviorEvents[ActiveRecord::EVENT_BEFORE_UPDATE] = 'updatedBy';
+        }
+
+        if ($this->recordWhileCreate) {
+            $behaviorEvents[ActiveRecord::EVENT_BEFORE_DELETE] = 'deletedBy';
+        }
+
+        return $behaviorEvents;
+    }
+
+    public function createdBy()
+    {
+        $attribute               = $this->createdAttribute;
+        $this->owner->$attribute = IdentityHelper::getUserId();
+    }
+
+    public function updatedBy()
+    {
+        $attribute               = $this->updatedAttribute;
+        $this->owner->$attribute = IdentityHelper::getUserId();
+    }
+
+    public function deletedBy()
+    {
+        $attribute               = $this->deletedAttribute;
+        $this->owner->$attribute = IdentityHelper::getUserId();
+    }
 
 }
